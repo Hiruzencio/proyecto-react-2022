5490f00fbf280d2c66db172464c55f7065e4e0e0
Diego <dbustamante@wiseconn.cl>
2023-01-07 22:56:29 -0300

    Se creo lista de criptos, se agrego axios, se creo archivo css

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,37 @@
-import { useState } from 'react'
-
+import { useEffect, useState } from 'react'
+import axios from 'axios';
+import "./App.css"
+  
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [criptos, setCriptos] = useState();
+  const API_URL = `${import.meta.env.VITE_API_URL}assets`;
+  useEffect(() => {
+    axios.get(API_URL)
+    .then((data) => {
+      console.log(data.data.data);
+      setCriptos(data.data.data);
+    })
+    .catch(() => {
+      console.error("La petición fallo");
+    })
+  }, []);
+
+  //const [count, setCount] = useState(0)
+  console.log(!criptos);
+  if(!criptos) {
+    return <span>Cargando...</span>
+  }
 
   return (
-    <h1>Hola mundo</h1>
+    <>
+      <h1>Lista de criptos</h1>
+      <ol>
+          { criptos.map(({id, name, priceUsd}) => (
+            <li key={id}>Nombre: {name} Precio: {priceUsd}</li>
+          ))}
+      </ol>
+    </>
   )
 }
 
